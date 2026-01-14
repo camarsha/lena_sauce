eeb0b3d5fae0a1b5002cf9fc894fd5edb1891bce
Caleb <marshallc@ohio.edu>
2026-01-14 11:44:23 -0500

    Added maps for other detectors. Changed internals a bit.

--- lena_sauce/detector_setup.py
+++ lena_sauce/detector_setup.py
@@ -8,11 +8,16 @@ import importlib.resources
 
 nai_map = importlib.resources.files("lena_sauce") / "NaI-Map.csv"
 scint_map = importlib.resources.files("lena_sauce") / "Scint-Map.csv"
+hpge_map = importlib.resources.files("lena_sauce") / "HPGe-Map.csv"
+
+nai_df = pl.read_csv(nai_map)
+scint_df = pl.read_csv(scint_map)
+hpge_df = pl.read_csv(hpge_map)
 
 
 def make_annulus(
     run_data,
-    map_file=nai_map,
+    map_df=nai_df,
     union=False,
 ):
     """Create the annulus detector using the map file.
@@ -24,20 +29,19 @@ def make_annulus(
 
     det_list = []
 
-    with open(map_file, "r") as f:
-        next(f)  # skip the header
-        for line in tqdm(f):
-            line = line.split()
-            if line:
-                temp = sauce.Detector(line[2])  # use the name
-                temp.find_hits(
-                    run_data, module=int(line[0]), channel=int(line[1])
-                )  # find the events
-                # drop useless stuff for now
-                num = int(re.split("(\d+)", temp.name)[1])
-                temp.tag(num, tag_name="nai_segment")
-                temp.primary_energy_col = "long"
-                det_list.append(temp)
+    for row in tqdm(map_df.iter_rows(named=True)):
+        temp = sauce.Detector(row["name"])
+        temp.find_hits(
+            run_data,
+            module=int(row["module"]),
+            channel=int(row["channel"]),
+        )
+        # drop useless stuff for now
+        num = int(re.split("(\\d+)", temp.name)[1])
+        temp.tag(num, tag_name="nai_segment")
+        temp.primary_energy_col = "long"
+        det_list.append(temp)
+
     # now make a union and return
     if union:
         return sauce.detector_union("nai", *det_list)
@@ -47,7 +51,7 @@ def make_annulus(
 
 def make_muon_veto(
     run_data,
-    map_file=scint_map,
+    map_df=scint_df,
     muon_thresholds=None,
     union=False,
 ):
@@ -60,25 +64,43 @@ def make_muon_veto(
 
     det_list = []
 
-    with open(map_file, "r") as f:
-        next(f)  # skip the header
-        for line in tqdm(f):
-            line = line.split()
-            if line:
-                temp = sauce.Detector(line[2])  # use the name
-                temp.find_hits(
-                    run_data, module=int(line[0]), channel=int(line[1])
-                )  # find the events
-                num = int(re.split("(\d+)", temp.name)[1])
-                temp.tag(num, tag_name="PS_segment")
-                if muon_thresholds:
-                    temp.apply_threshold(int(line[3]), col="long")
-                det_list.append(temp)
+    for row in tqdm(map_df.iter_rows(named=True)):
+        temp = sauce.Detector(row["name"])
+        temp.find_hits(run_data, module=int(row["module"]), channel=int(row["channel"]))
+        num = int(re.split("(\\d+)", temp.name)[1])
+        temp.tag(num, tag_name="PS_segment")
+
+        if muon_thresholds:
+            # Assuming threshold value is in fourth column
+            temp.apply_threshold(int(row[map_df.columns[3]]), col="long")
+        det_list.append(temp)
+
     # now make a union and return
     if union:
         return sauce.detector_union("PS", *det_list)
     else:
         return det_list
+
+
+def make_hpge(
+    run_data,
+    map_df=hpge_df,
+):
+    """This function picks up the rest of the detectors present in the system.
+
+    :param run_data: Run data
+    :param map_df: Polars DataFrame
+    :returns: Dictionary of Detector objects.
+    """
+
+    det_dic = {}
+
+    for row in tqdm(map_df.iter_rows(named=True)):
+        temp = sauce.Detector(row["name"])
+        temp.find_hits(run_data, module=int(row["module"]), channel=int(row["channel"]))
+        det_dic[temp.name] = temp
+
+    return det_dic
 
 
 def nai_sum(
@@ -113,12 +135,21 @@ def make_all_detectors(run_data):
     caller_globals["nai"] = nai
     scint = make_muon_veto(run_data)
     caller_globals["scint"] = scint
-    hpge = sauce.Detector("hpge").find_hits(run_data, module=225, channel=0)
-    caller_globals["hpge"] = hpge
-    hpge_t = sauce.Detector("hpge_t").find_hits(run_data, module=225, channel=2)
-    caller_globals["hpge_t"] = hpge_t
-    pulser = sauce.Detector("pulser").find_hits(run_data, module=225, channel=4)
-    caller_globals["pulser"] = pulser
-    beam = sauce.Detector("beam").find_hits(run_data, module=225, channel=6)
-    caller_globals["beam"] = beam
+    dets = make_hpge(run_data)
+    hpge = None
+    hpge_t = None
+    pulser = None
+    beam = None
+    if "hpge" in dets:
+        hpge = dets["hpge"]
+        caller_globals["hpge"] = hpge
+    if "hpge_t" in dets:
+        hpge_t = dets["hpge_t"]
+        caller_globals["hpge_t"] = hpge_t
+    if "pulser" in dets:
+        pulser = dets["pulser"]
+        caller_globals["pulser"] = pulser
+    if "beam" in dets:
+        beam = dets["beam"]
+        caller_globals["beam"] = beam
     return hpge, hpge_t, pulser, beam, nai, scint
